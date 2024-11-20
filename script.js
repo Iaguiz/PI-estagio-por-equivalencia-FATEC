71545c62982566a07ad57bc82b320f8811dd2c96
Christian Andrade <andrade.dias.christian@gmail.com>
2024-11-20 19:23:54 -0300

    create modal login

---
+++ script.js
@@ -0,0 +1,5 @@
+const handleCreateAccount = () => {
+    const modal = document.getElementById('modal-container');
+
+    modal.style.display =  (modal.style.display === 'none' || !modal.style.display) ? 'flex' : 'none';
+}
