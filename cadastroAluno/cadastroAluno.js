104dfc8fcf547091751494c2fefb525d18cf061b
Iago Reis Homsi <iagohomsi@gmail.com>
2024-11-20 20:15:00 -0300

    Pagina de cadastro responsiva até 800px, falta fazer até 250px

--- cadastroAluno/cadastroAluno.js
+++ cadastroAluno/cadastroAluno.js
@@ -4,25 +4,29 @@
 function handleName(){
     const nome = document.getElementById('nome').value;
     console.log(nome);
-
 }
+
 function handleEmail(){
     const email = document.getElementById('email').value;
     console.log(email);
-
 }
+
 function handleCPF(){
     const CPF = document.getElementById('CPF').value;
     console.log(CPF);
-
 }
+
 function handleRA(){
     const RA = document.getElementById('RA').value;
     console.log(RA);
-
 }
+
 function handleUnidade(){
     const unidade = document.getElementById('unidade').value;
     console.log(unidade);
+}
 
+function handleCurso(){
+    const curso = document.getElementById('curso').value;
+    console.log(curso);
 }
