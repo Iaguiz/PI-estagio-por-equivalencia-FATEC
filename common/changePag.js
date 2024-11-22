5e2bce9bd1fd00de7eba482dcf1121a68b42cb46
chr1s <chris@chris.com>
2024-11-22 19:43:03 -0300

    adjust modal

---
+++ common/changePag.js
@@ -0,0 +1,8 @@
+const changePage = (pathParam1, pathParam2) => {
+    const URLAtual = window.location.href.split('/');
+    
+    URLAtual[URLAtual.length - 2] = pathParam1;
+    URLAtual[URLAtual.length - 1] = pathParam2;
+
+    window.location = URLAtual.join('/');  
+}
